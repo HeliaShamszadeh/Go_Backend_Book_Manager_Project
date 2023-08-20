d7b3ad5a5d9df350f688de2e378bf720c9f0da57
Helia <shamszadehhelia@gmail.com>
2023-08-20 23:02:02 +0330

    added some authenticate functions

--- authenticate/authenticate.go
+++ authenticate/authenticate.go
@@ -1,2 +1,86 @@
 package authenticate
 
+import (
+	"bookman/db"
+	"crypto/rand"
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type Authenticate struct {
+	database              *db.GormDB
+	logger                *logrus.Logger
+	secretKey             []byte
+	jwtExpirationDuration time.Duration
+}
+
+type Credential struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	TokenString string
+}
+
+type claims struct {
+	jwt.MapClaims
+	Username string `json:"username"`
+}
+
+func NewAuth(db *db.GormDB, jwtExpirationDuration time.Duration, logger *logrus.Logger) (*Authenticate, error) {
+	secretKey, err := generateRandomKey()
+	if err != nil {
+		return nil, err
+	}
+
+	// Check database
+	if db == nil {
+		return nil, errors.New("database can not be nil")
+	}
+
+	return &Authenticate{
+		database:              db,
+		secretKey:             secretKey,
+		jwtExpirationDuration: jwtExpirationDuration,
+		logger:                logger,
+	}, nil
+}
+
+func (a *Authenticate) Login(cred Credential) (*Token, error) {
+	account, err := a.database.GetUserByUsername(cred.Username)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check password
+	err = bcrypt.CompareHashAndPassword([]byte(account.Password), []byte(cred.Password))
+	if err != nil {
+		return nil, errors.New("the password is not correct")
+	}
+
+	// Create JWT token
+	expirationTime := time.Now().Add(a.jwtExpirationDuration)
+	tokenJWT := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims{
+		Username: cred.Username,
+		MapClaims: jwt.MapClaims{
+			"expired_at": expirationTime.Unix(),
+		},
+	})
+	tokenString, err := tokenJWT.SignedString(a.secretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Token{TokenString: tokenString}, err
+}
+func generateRandomKey() ([]byte, error) {
+	jwtKey := make([]byte, 32)
+	if _, err := rand.Read(jwtKey); err != nil {
+		return nil, err
+	}
+	return jwtKey, nil
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module bookman
 go 1.20
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/crypto v0.8.0
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.4
 )
@@ -19,7 +21,6 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
-	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
